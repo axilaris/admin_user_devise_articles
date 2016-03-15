f9f2f4ca968b680a402d14b60f6a6e75d5c31e62
firdaus <firdaus.mah@gmail.com>
2016-03-16 06:25:25 +0800

    fix sign_out redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,24 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
 	  if resource.is_a?(User)
-	    #user_articles_path
 	    user_dashboard_index_path
 	  elsif resource.is_a?(Admin)
-	    #admin_articles_path
 	    admin_dashboard_index_path
 	  end
 	end  	
 
 
 	def after_sign_out_path_for(resource)
-	  if resource.is_a?(User)
-	    new_user_session_path
-	  elsif resource.is_a?(Admin)
-	    new_admin_session_path
+	 case resource
+	    when :user, User
+	      new_user_session_path
+	    when :admin, Admin
+	      new_admin_session_path
+	    else
+	      super
 	  end
+
+
 	end  	
 
 end
